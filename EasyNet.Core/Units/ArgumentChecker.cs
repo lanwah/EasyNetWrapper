989afbe4bd18b19cec81da2984dd9be2cf6acaf0
lanwah <1051573215@qq.com>
2021-02-22 16:09:37 +0800

    #添加参数检查类

---
+++ EasyNet.Core/Units/ArgumentChecker.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+// ------------------------------------------------------------- //
+// 版权所有：CopyRight (C) lanwah
+// 项目名称：EasyNet.Core
+// 文件名称：ArgsChecker
+// 创 建 者：lanwah
+// 创建日期：2021/02/22 15:21:57
+// 功能描述：
+// 调用依赖：
+// -------------------------------------------------------------
+// 修 改 者：
+// 修改时间：
+// 修改原因：
+// 修改描述：
+// ------------------------------------------------------------- //
+
+namespace EasyNet.Core
+{
+    /// <summary>
+    /// 参数检测，不符合检测结果时抛出异常
+    /// </summary>
+    public static partial class ArgumentChecker
+    {
+        /// <summary>
+        /// 判断参数是否非空，空时抛出ArgumentNullException
+        /// </summary>
+        /// <param name="argumentValue">参数值</param>
+        /// <param name="argumentName">参数名称，可以通过nameof(argumentValue)进行使用</param>
+        public static void NotNull(object argumentValue, string argumentName)
+        {
+            if (argumentValue == null)
+            {
+                throw new ArgumentNullException(argumentName);
+            }
+        }
+        /// <summary>
+        /// 判断参数是否非空，空时抛出ArgumentNullException
+        /// </summary>
+        /// <param name="argumentValue"></param>
+        /// <param name="argumentName"></param>
+        public static void NotNullOrEmpty(string argumentValue, string argumentName)
+        {
+            if ((argumentValue == null) || (argumentValue.Length == 0))
+            {
+                throw new ArgumentNullException(argumentName);
+            }
+        }
+    }
+}
--- README.md
+++ README.md
@@ -2,6 +2,17 @@
 # 证书地址
 https://choosealicense.com/licenses/mit/#  
 
+# 项目结构
+-- Configs
+  |-- log4net.config
+-- Controls
+-- Extension
+-- Units
+  |-- ArgumentChecker.cs
+-- LogProvider.cs
+-- ServiceSingleton.cs
+
 # 2020-11-18 lanwah  
 * 1、添加了 ServiceSingleton 服务提供程序管理类；    
-* 2、添加了 LogProvider 默认使用 log4net的日志功能；  
+* 2、添加了 LogProvider 默认使用 log4net的日志功能； 
+* 3、添加了 ArgumentChecker 参数检查类； 
