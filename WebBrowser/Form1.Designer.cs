540a4f7b5cef2208efb58f56dd95abe497340b00
lanwah <1051573215@qq.com>
2024-07-29 18:16:06 +0800

    #10 添加WebBrowser项目，用于测试更改WinForm中WebBrowser控件使用的IE版本

---
+++ WebBrowser/Form1.Designer.cs
@@ -0,0 +1,137 @@
+﻿namespace WebBrowser
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// 必需的设计器变量。
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// 清理所有正在使用的资源。
+        /// </summary>
+        /// <param name="disposing">如果应释放托管资源，为 true；否则为 false。</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows 窗体设计器生成的代码
+
+        /// <summary>
+        /// 设计器支持所需的方法 - 不要修改
+        /// 使用代码编辑器修改此方法的内容。
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.panel1 = new System.Windows.Forms.Panel();
+            this.panel2 = new System.Windows.Forms.Panel();
+            this.label1 = new System.Windows.Forms.Label();
+            this.txtUrl = new System.Windows.Forms.TextBox();
+            this.btnGo = new System.Windows.Forms.Button();
+            this.webBrowser1 = new System.Windows.Forms.WebBrowser();
+            this.btnV = new System.Windows.Forms.Button();
+            this.panel1.SuspendLayout();
+            this.panel2.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // panel1
+            // 
+            this.panel1.Controls.Add(this.btnV);
+            this.panel1.Controls.Add(this.btnGo);
+            this.panel1.Controls.Add(this.txtUrl);
+            this.panel1.Controls.Add(this.label1);
+            this.panel1.Dock = System.Windows.Forms.DockStyle.Top;
+            this.panel1.Location = new System.Drawing.Point(0, 0);
+            this.panel1.Name = "panel1";
+            this.panel1.Size = new System.Drawing.Size(821, 56);
+            this.panel1.TabIndex = 0;
+            // 
+            // panel2
+            // 
+            this.panel2.Controls.Add(this.webBrowser1);
+            this.panel2.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.panel2.Location = new System.Drawing.Point(0, 56);
+            this.panel2.Name = "panel2";
+            this.panel2.Size = new System.Drawing.Size(821, 459);
+            this.panel2.TabIndex = 1;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(13, 22);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(65, 12);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "浏览地址：";
+            // 
+            // txtUrl
+            // 
+            this.txtUrl.Location = new System.Drawing.Point(84, 18);
+            this.txtUrl.Name = "txtUrl";
+            this.txtUrl.Size = new System.Drawing.Size(526, 21);
+            this.txtUrl.TabIndex = 1;
+            this.txtUrl.Text = "https://ie.icoa.cn/";
+            // 
+            // btnGo
+            // 
+            this.btnGo.Location = new System.Drawing.Point(632, 17);
+            this.btnGo.Name = "btnGo";
+            this.btnGo.Size = new System.Drawing.Size(75, 23);
+            this.btnGo.TabIndex = 2;
+            this.btnGo.Text = "打开";
+            this.btnGo.UseVisualStyleBackColor = true;
+            this.btnGo.Click += new System.EventHandler(this.btnGo_Click);
+            // 
+            // webBrowser1
+            // 
+            this.webBrowser1.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.webBrowser1.Location = new System.Drawing.Point(0, 0);
+            this.webBrowser1.MinimumSize = new System.Drawing.Size(20, 20);
+            this.webBrowser1.Name = "webBrowser1";
+            this.webBrowser1.Size = new System.Drawing.Size(821, 459);
+            this.webBrowser1.TabIndex = 0;
+            // 
+            // btnV
+            // 
+            this.btnV.Location = new System.Drawing.Point(713, 17);
+            this.btnV.Name = "btnV";
+            this.btnV.Size = new System.Drawing.Size(75, 23);
+            this.btnV.TabIndex = 3;
+            this.btnV.Text = "Version";
+            this.btnV.UseVisualStyleBackColor = true;
+            this.btnV.Click += new System.EventHandler(this.btnV_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 12F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(821, 515);
+            this.Controls.Add(this.panel2);
+            this.Controls.Add(this.panel1);
+            this.Name = "Form1";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Form1";
+            this.panel1.ResumeLayout(false);
+            this.panel1.PerformLayout();
+            this.panel2.ResumeLayout(false);
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Panel panel1;
+        private System.Windows.Forms.Button btnGo;
+        private System.Windows.Forms.TextBox txtUrl;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Panel panel2;
+        private System.Windows.Forms.WebBrowser webBrowser1;
+        private System.Windows.Forms.Button btnV;
+    }
+}
+
---
+++ WebBrowser/Form1.cs
@@ -0,0 +1,82 @@
+﻿using Microsoft.Win32;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Diagnostics;
+using System.Drawing;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace WebBrowser
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void btnGo_Click(object sender, EventArgs e)
+        {
+            this.webBrowser1.Navigate(txtUrl.Text);
+        }
+
+        private void btnV_Click(object sender, EventArgs e)
+        {
+            MessageBox.Show(this.webBrowser1.Version.ToString());
+        }
+
+        /// <summary>
+        /// 未测试，待验证
+        /// </summary>
+        /// <param name="major"></param>
+        private void UseLatestIE(int major)
+        {
+            try
+            {
+                int num = 0;
+                switch (major)
+                {
+                    case 7:
+                        // 0x1B58
+                        num = 7000;
+                        break;
+                    case 8:
+                        // 0x22B8
+                        num = 8888;
+                        break;
+                    case 9:
+                        // 0x270F
+                        num = 9999;
+                        break;
+                    case 10:
+                        // 0x2711
+                        num = 10001;
+                        break;
+                    case 11:
+                        // 0x2AF9
+                        num = 11001;
+                        break;
+                }
+
+                if (num != 0)
+                {
+                    using (var registryKey = Registry.CurrentUser.OpenSubKey("SOFTWARE\\Microsoft\\Internet Explorer\\Main\\FeatureControl\\FEATURE_BROWSER_EMULATION", true))
+                    {
+                        if (registryKey != null)
+                        {
+                            registryKey.SetValue(Path.GetFileName(Process.GetCurrentProcess().MainModule.FileName), num, RegistryValueKind.DWord);
+                        }
+                    }
+                }
+            }
+            catch
+            {
+            }
+        }
+    }
+}
